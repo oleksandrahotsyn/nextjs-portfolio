57e6a9bcfb6046c07bf07bbd7ff38bf75a547d99
Sandra Hotsyn <sandrahotsy@gmail.com>
2026-06-13 23:12:30 +0300

    refix fon mobil

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,12 @@ import TechStack from "@/components/techstack/TechStack";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-    <main>
+    <>
         <Hero />
         <Services />
         <TechStack />
         <Projects />
-    </main>
+    </>
     </div>
   );
 }
