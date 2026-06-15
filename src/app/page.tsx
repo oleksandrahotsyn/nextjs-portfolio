c82273884960434c9033c9ec4d79eb5f90d829e4
Sandra Hotsyn <sandrahotsy@gmail.com>
2026-06-15 19:29:29 +0300

    fix style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,11 @@ import TechStack from "@/components/techstack/TechStack";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
     <>
         <Hero />
         <Services />
         <TechStack />
         <Projects />
     </>
-    </div>
   );
 }
